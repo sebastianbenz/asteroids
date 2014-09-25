b30014b1a69d9d242e8fa3815e95e98c226482b5
Sebastian Benz <sebastian.benz@gmail.com>
2014-09-25 20:20:47 +0200

    add java project info

--- java/src/TriangleTest.java
+++ java/src/TriangleTest.java
@@ -45,24 +45,17 @@ public class TriangleTest {
                     new Point2D.Double(100, 100),
                     new Point2D.Double(0, 100)
             );
-
-            poly = new Polygon(
-                    new int[]{50, 100, 0},
-                    new int[]{0, 100, 100},
-                    3);
         }
 
         @Override
         public Dimension getPreferredSize() {
-            return new Dimension(200, 200);
+            return new Dimension(800, 600);
         }
 
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
             Graphics2D g2d = (Graphics2D) g.create();
-            g2d.setColor(Color.RED);
-            g2d.fill(poly);
 
             g2d.setColor(Color.GREEN);
             g2d.translate(50, 100);
